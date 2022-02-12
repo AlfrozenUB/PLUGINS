96de300824b07c0f2bcb6214b11867ed09bbd293
THE-ROYALSBOT <97783417+THE-ROYALSBOT@users.noreply.github.com>
2022-02-12 21:30:18 +0530

    Add files via upload

---
+++ ROYALBOT/start.sh
@@ -0,0 +1,36 @@
+#   Copyright (C) 2021 THE-ROYALSBOT
+#
+#   Poject VΛDΣЯ is free software: 
+#   you can redistribute it and/or modify it under the terms of the GNU General Public License 
+#   as published by the Free Software Foundation, either version 3 of the License, 
+#   or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful, 
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+#   See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along with this program. 
+#   If not, see <https://www.gnu.org/licenses/>.
+#
+
+
+echo """
+ 
+
+╭━━━┳━━━┳╮╱╱╭┳━━━┳╮
+┃╭━╮┃╭━╮┃╰╮╭╯┃╭━╮┃┃
+┃╰━╯┃┃╱┃┣╮╰╯╭┫┃╱┃┃┃
+┃╭╮╭┫┃╱┃┃╰╮╭╯┃╰━╯┃┃╱╭╮
+┃┃┃╰┫╰━╯┃╱┃┃╱┃╭━╮┃╰━╯┃
+╰╯╰━┻━━━╯╱╰╯╱╰╯╱╰┻━━━╯
+╭╮╱╭┳━━━┳━━━┳━━━┳━━╮╭━━━┳━━━━╮
+┃┃╱┃┃╭━╮┃╭━━┫╭━╮┃╭╮┃┃╭━╮┃╭╮╭╮┃
+┃┃╱┃┃╰━━┫╰━━┫╰━╯┃╰╯╰┫┃╱┃┣╯┃┃╰╯
+┃┃╱┃┣━━╮┃╭━━┫╭╮╭┫╭━╮┃┃╱┃┃╱┃┃
+┃╰━╯┃╰━╯┃╰━━┫┃┃╰┫╰━╯┃╰━╯┃╱┃┃
+╰━━━┻━━━┻━━━┻╯╰━┻━━━┻━━━╯╱╰╯
+"""
+rm -rf ROYALUSERBOT
+git clone https://github.com/THE-ROYALSBOT/ROYALUSERBOT
+cd ROYALUSERBOT
+python3 -m userbot
